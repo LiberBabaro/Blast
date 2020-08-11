5ac4c6d6930755ffed8a6b11911f6eed3d118512
Ilya Soldatkin <soldatkinilya1996@gmail.com>
2020-08-11 23:56:18 +0500

    Add font Marvin

--- src/resource.js
+++ src/resource.js
@@ -1,7 +1,14 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
     CloseNormal_png : "res/CloseNormal.png",
-    CloseSelected_png : "res/CloseSelected.png"
+    CloseSelected_png : "res/CloseSelected.png",
+
+    font: {
+        type: "font",
+        name: "Marvin",
+        srcs: ["res/Marvin.ttf"]
+    }
+
 };
 
 var g_resources = [];
