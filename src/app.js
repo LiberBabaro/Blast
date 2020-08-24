967370c7fcba7107c85755d2426df35b882361e2
Ilya Soldatkin <soldatkinilya1996@gmail.com>
2020-08-24 20:09:31 +0500

    gamescene added score labels and score callback

--- src/app.js
+++ src/app.js
@@ -1,22 +1,14 @@
 
-var HelloWorldLayer = cc.Layer.extend({
+var GameLayer = cc.Layer.extend({
     sprite:null,
     ctor:function () {
-        //////////////////////////////
-        // 1. super init first
         this._super();
 
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
         var size = cc.winSize;
 
         var backgroundLayer = new cc.LayerColor.create(new cc.color(161, 161, 161));
-        //backgroundLayer.setColor(cc.color(161, 161, 161));
         this.addChild(backgroundLayer);
-        
-        // add a "close" icon to exit the progress. it's an autorelease object
+
         var closeItem = new cc.MenuItemImage(
             res.CloseNormal_png,
             res.CloseSelected_png,
@@ -35,81 +27,95 @@ var HelloWorldLayer = cc.Layer.extend({
         menu.y = 0;
         this.addChild(menu, 1);
 
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-
-
-        // add "HelloWorld" splash screen
         this.field = new Field();
-        //this.field.setPosition(cc.p( ((size.width - 1641) / 2), (size.height - 1827) / 2) );
         this.field.setPosition(cc.p( (132), (219)) );
         this.addChild( this.field );
 
-        //this.field.runAction(
-        //    cc.spawn(
-        //        cc.moveTo(2.5, cc.p(0, 0))
-        //    )
-        //);
-
+        this.scoreCount = 0;
+        this.movesCount = 37;
         this.score = new cc.Sprite(res.ScoreBg);
         this.score.attr({
             x: 2197 - 29 + this.score.width / 2,
             y: 922 - 45 + this.score.height / 2
         });
+
+        var timeLabel = new cc.LabelTTF("Время:", "Marvin", 65);
+        timeLabel.x = this.score.width / 2;
+        timeLabel.y = 1123;
+        this.score.addChild(timeLabel, 5);
+
+        var scoreLabel = new cc.LabelTTF("Очки:", "Marvin", 65);
+        scoreLabel.x = this.score.width / 2;
+        scoreLabel.y = 361;
+        this.score.addChild(scoreLabel, 5);
+
+        var scoreCountLabel = new cc.LabelTTF("0", "Marvin", 132);
+        scoreCountLabel.x = this.score.width / 2;
+        scoreCountLabel.y = 235;
+        this.score.addChild(scoreCountLabel, 5);
+
+        var movesCountLabel = new cc.LabelTTF("37", "Marvin", 230);
+        movesCountLabel.x = this.score.width / 2;
+        movesCountLabel.y = 712;
+        this.score.addChild(movesCountLabel, 5);
+
         this.addChild(this.score, 0);
+
+        this.pauseBtn = new cc.Sprite(res.button.pause);
+        this.pauseBtn.attr({
+            x: 3197 - 30,
+            y: 2220 - 48
+        });
+        this.pauseBtn.setAnchorPoint( cc.p( 0, 0 ) );
+
+        this.addChild(this.pauseBtn, 0);
 
         this.top = new cc.Sprite(res.TopBg);
         this.top.attr({
             x: 310 - 5,
             y: 2165 - 14
         });
-        //this.top.setAnchorPoint( cc.p( - 623, - 314 ) );
+
         this.progress = new cc.Sprite(res.progress.back);
         this.top.setAnchorPoint( cc.p( 0, 0 ) );
         this.progress.attr({
             x: 625,
             y: 65
         });
+
         this.progress.setAnchorPoint( cc.p( 0, 0 ) );
+
         var progressLabel = new cc.LabelTTF("Прогресс", "Marvin", 64);
-        // position the label on the center of the screen
         progressLabel.x = 507 + progressLabel.width / 2;
         progressLabel.y = 170 + progressLabel.height / 2;
-        // add the label as a child to this layer
         this.progress.addChild(progressLabel, 5);
+
         this.top.addChild(this.progress, 3);
         this.addChild(this.top, 2);
-        //this.sprite = new cc.Sprite(res.tile.red);
-        //this.sprite.attr({
-        //    x: size.width / 2,
-        //    y: size.height / 2,
-        //    scale: 2,
-        //    rotation: 180
-        //});
-        //this.addChild(this.sprite, 0);
-
-        //this.sprite.runAction(
-        //    cc.sequence(
-        //        cc.rotateTo(2, 0),
-        //        cc.scaleTo(2, 1, 1)
-        //    )
-        //  );
-        //helloLabel.runAction(
-        //    cc.spawn(
-        //        cc.moveBy(2.5, cc.p(0, size.height - 40)),
-        //        cc.tintTo(2.5,255,125,0)
-        //    )
-        //);
         return true;
+    },
+
+    updateScoreLabel: function() {
+        this.scoreLabel.setString( this.score );
+        if (this.scoreCount == 299){}
+        if (this.movesCount == 0){}
+            //cc.director.runScene(new GameOverScene(this.score));
+    },
+
+    registerScoreCallback: function() {
+        var gameLayer = this;
+        this.field.setEatCallback(function( coin ) {
+            gameLayer.scoreCount++;
+            gameLayer.movesCount--;
+            gameLayer.updateScoreLabel();
+        });
     }
 });
 
-var HelloWorldScene = cc.Scene.extend({
+var GameScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
-        var layer = new HelloWorldLayer();
+        var layer = new GameLayer();
         this.addChild(layer);
     }
 });
